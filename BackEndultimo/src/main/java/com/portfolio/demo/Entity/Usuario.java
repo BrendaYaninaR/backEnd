b90481ec8853c35e2d2e6704f7f3340da5ba105b
BrendaYaninaR <87450843+BrendaYaninaR@users.noreply.github.com>
2022-07-31 23:42:08 -0300

    Add files via upload

---
+++ BackEndultimo/src/main/java/com/portfolio/demo/Entity/Usuario.java
@@ -0,0 +1,35 @@
+package com.portfolio.demo.Entity;
+
+import com.portfolio.demo.Security.Enums.Entity.Rol;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotNull
+    private String nombre;
+    @NotNull
+    @Column(unique = true)
+    private String nombreUsuario;
+    @NotNull
+    private String email;
+    @NotNull
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "usuario_rol", joinColumns = @JoinColumns(name = "usuario_id"),inverseJoinColumns = @JoinColumn(name ="rol_id"))
+    private final Set<Rol> roles =new HashSet<>();
+}
